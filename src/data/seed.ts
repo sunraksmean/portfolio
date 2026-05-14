14c345e48e5ce20d91af56e1e26309bc4ade9714
sunraksmean <sunraksmean@gmail.com>
2026-05-14 16:53:50 +0700

    Update seed data via UI

--- src/data/seed.ts
+++ src/data/seed.ts
@@ -135,32 +135,6 @@ export const defaultExperience: Experience[] = [
     ]
   },
   {
-    "id": "exp-1778746693357",
-    "company": "KongVong Finance",
-    "role": "Sharehoder  and Owner",
-    "period": "",
-    "duration": "",
-    "description": [
-      "Installed, configured and maintained computers, printers, network devices and office software",
-      "Provided helpdesk support to staff (hardware, software, network and user accounts)",
-      "Managed user access, passwords and permissions",
-      "Monitored and maintained office network, Wi-Fi and internet connectivity",
-      "Performed system backup and data recovery",
-      "Installed and supported business applications used by finance and operations",
-      "Maintained company website and basic hosting/deployment",
-      "Troubleshoot technical issues quickly to minimize downtime",
-      "Supported daily use of accounting and financial systems",
-      "Assisted finance team with:",
-      "System setup and troubleshooting",
-      "Data entry and system validation",
-      "Generating reports from systems",
-      "Helped ensure data accuracy and system reliability",
-      "Supported digital record keeping and document management"
-    ],
-    "current": false,
-    "descText": "Installed, configured and maintained computers, printers, network devices and office software\nProvided helpdesk support to staff (hardware, software, network and user accounts)\nManaged user access, passwords and permissions\nMonitored and maintained office network, Wi-Fi and internet connectivity\nPerformed system backup and data recovery\nInstalled and supported business applications used by finance and operations\nMaintained company website and basic hosting/deployment\nTroubleshoot technical issues quickly to minimize downtime\nSupported daily use of accounting and financial systems\nAssisted finance team with:\nSystem setup and troubleshooting\nData entry and system validation\nGenerating reports from systems\nHelped ensure data accuracy and system reliability\nSupported digital record keeping and document management"
-  },
-  {
     "id": "exp-1778736273718",
     "company": "Jek Meas168 Hang Banhcham",
     "role": "IT Support – Finance & Operations Support",
